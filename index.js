b1b4e41e2a9ed15ca5012e07d1f6cb2384e991a7
luoct <1422291209@qq.com>
2020-04-25 14:06:41 +0800

    l

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+
+let app = express();
+
+// app.engine('.html', require('express-art-template'));
+// app.set('view engine', '.html');
+
+// let router = express.Router();
+// app.use('/', router)
+
+app.use('/public', express.static('./public'))
+
+app.get('/timeline', (req, res) => {
+    res.sendFile(__dirname + '/views/timeline.html')
+})
+app.get('/411', (req, res) => {
+    res.sendFile(__dirname + '/views/411.html')
+})
+app.get('/413', (req, res) => {
+    res.sendFile(__dirname + '/views/413.html')
+})
+app.get('/417', (req, res) => {
+    res.sendFile(__dirname + '/views/417.html')
+})
+app.get('/423', (req, res) => {
+    res.sendFile(__dirname + '/views/423.html')
+})
+
+app.all('*', (req, res) => {
+    res.sendFile(__dirname + '/views/errrr.html')
+})
+
+
+app.listen(3333)
+
