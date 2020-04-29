c66d4b8e0f59a9077cbfd18ee7ba5660b4d6159c
luoct <1422291209@qq.com>
2020-04-29 15:28:01 +0800

    ico

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./db')
+var favicon = require('serve-favicon')
 
 let app = express();
 
@@ -11,14 +12,16 @@ let app = express();
 // error: 在router中sendFile中的html文件无法访问public中的静态资源
 
 app.use('/public', express.static('./public'))
+// express 4.x中需要自己去添加中间件
+app.use(favicon('favicon.ico'))
 
 // 解析请求
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/views/timeline.html')
-// })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/timeline.html')
+})
 // app.get('/411', (req, res) => {
 //     res.sendFile(__dirname + '/views/411.html')
 // })
@@ -33,13 +36,14 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 // })
 
 
-app.get('/:data', (req, res) => {
-    res.sendFile(__dirname + '/views/' + req.params.data + '.html')
+// 根据访问的路劲自动去判断文件
+app.get('/mails/:date', (req, res) => {
+    res.sendFile(__dirname + '/views/' + req.params.date + '.html')
 })
 
 // reply的 get、post 请求
 app.get('/reply', (req, res) => {
-
+    res.send(`<textarea>reply</textarea>`)
 })
 app.post('/reply', (req, res) => {
     res.set("Access-Control-Allow-Origin", "*")
@@ -48,12 +52,12 @@ app.post('/reply', (req, res) => {
     // 存入mongodb中
 
     // res返回状态。（并重定向至回信页面）
-    res.send('ok')
+    res.send({ status: 'ok', data: req.body })
 })
 
-// app.all('*', (req, res) => {
-//     res.sendFile(__dirname + '/views/errrr.html')
-// })
+app.all('*', (req, res) => {
+    res.sendFile(__dirname + '/views/errrr.html')
+})
 
 
 app.listen(3333)
