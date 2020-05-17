50b7f0f91c6cbd5cd8aa52af1cf6a01c3835b92a
luoct <1422291209@qq.com>
2020-05-17 17:19:25 +0800

    add some sentence

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let app = express();
 // let router = express.Router();
 // app.use('/', router)
 // error: 在router中sendFile中的html文件无法访问public中的静态资源
+// 猜想：在使用router前配置静态文件
 
 app.use('/public', express.static('./public'))
 // express 4.x中需要自己去添加中间件
@@ -28,4 +29,3 @@ app.all('*', (req, res) => {
 
 
 app.listen(3333)
-
